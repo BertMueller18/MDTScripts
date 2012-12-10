7444741db2c2429b5b37c17b89ebbd6de0e5ebfe
Segaso <lennykelley@gmail.com>
2012-12-09 19:49:52 -0500

    Added Scripts

---
+++ MDT - DomainOUList.ps1
@@ -0,0 +1,19 @@
+﻿$OrganizatioanlUnits = Get-AdOrganizationalUnit -Filter *
+$a = New-Object System.Collections.ArrayList
+FOREACH ($OU IN $OrganizatioanlUnits) {
+    $Name     = $OU.Name
+    $FullPath = $OU.DistinguishedName 
+    [void]$DomainOU.Add('<DomainOU value="' + $FullPath + '">' + $Name + '</DomainOU>' + "`n")
+    
+}
+
+
+$DomainOUList = @"
+<?xml version="1.0" encoding="utf-8"?>
+<DomainOUs>
+$DomainOU
+</DomainOUs>
+"@
+
+
+Out-File -InputObject $DomainOUList -FilePath C:\Users\$env:USERNAME\desktop\domainOUList.xml
---
+++ MDT Database - Bulk_Computers.ps1
@@ -0,0 +1,25 @@
+﻿Import-Module MDTDB
+
+
+
+
+$Database = @{
+    Instance  = "SQLExpress"
+    SQLServer = "RedHorse"
+    Database  = "MDT"
+}
+    Connect-MDTDatabase @Database 
+
+Import-Csv -Path $Path | ForEach-Object {
+    $Computer = @{
+        MACAddress   = "$_.MacAddress"
+        SerialNumber = "$_.SerialName"
+        Description  = "$_.ComputerName"
+        Settings     = @{
+                            OSInstall       = "YES"
+                            OSDComputerName = "$_.ComputerName"
+                        }
+        }
+            New-MDTComputer @Computer 
+
+}
